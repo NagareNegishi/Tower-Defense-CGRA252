59bec3da00fe00bd4b39cc1bd1a6b78f3bb70f36
negimakushi <132905416+negimakushi@users.noreply.github.com>
2024-09-16 18:43:57 +1200

    implemented GameStats

--- Tower-Defense-Game/Scripts/level_manager/game_stats.gd
+++ Tower-Defense-Game/Scripts/level_manager/game_stats.gd
@@ -1,18 +1,40 @@
 extends Node
 class_name GameStats
 
+## GameStats manages the game statistics such as score, life, and resources.
+## It uses signals to notify other parts of the game when these values change.
+
 var score: int = 0
 var life: int = 100
 var resources: int = 1000
 
+# HUD should listen to these signals
+signal score_changed(new_score: int)
+signal life_changed(new_life: int)
+signal resources_changed(new_resources: int)
+
+# Update the score
 func update_score(amount: int):
 	score += amount
 
+# Update the life
 func update_life(amount: int):
 	life -= amount
 
+# Update the resources
 func update_resources(amount: int):
 	resources += amount
+
+
+
+
+
+
+
+
+
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
