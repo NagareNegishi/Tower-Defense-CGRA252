5ff47b9411babdf9253fbd50049d6d88c1ff3c3b
BrockChoy <brockieeboyy@gmail.com>
2024-09-29 23:10:59 +1300

    I have got a rudimentary working version of the tower defense havent edited much at all and kept all old code there commented out for reference

--- Tower-Defense-Game/Scripts/level_manager/EnemyPath.gd
+++ Tower-Defense-Game/Scripts/level_manager/EnemyPath.gd
@@ -5,24 +5,24 @@ var path_follow: PathFollow2D
 var slime_scene = preload("res://Scenes/Prototype/Temp_Slime.tscn")
 
 func _ready():
-    path_follow = $PathFollow2D
-    path_follow.loop = false
+	path_follow = $PathFollow2D
+	path_follow.loop = false
 
 func get_start_position() -> Vector2:
-    return curve.get_point_position(0)
+	return curve.get_point_position(0)
 
 func get_end_position() -> Vector2:
-    return curve.get_point_position(curve.point_count - 1)
+	return curve.get_point_position(curve.point_count - 1)
 
 func spawn_enemy(enemy_scene: PackedScene) -> Node2D:
-    var enemy = enemy_scene.instantiate()
-    path_follow.add_child(enemy)
-    path_follow.progress_ratio = 0
-    return enemy
+	var enemy = enemy_scene.instantiate()
+	path_follow.add_child(enemy)
+	path_follow.progress_ratio = 0
+	return enemy
 
 
 func get_progress() -> float:
-    return path_follow.progress_ratio
+	return path_follow.progress_ratio
 
 func set_progress(value: float):
-    path_follow.progress_ratio = value
+	path_follow.progress_ratio = value
--- Tower-Defense-Game/Scripts/tower1Arrow.gd
+++ Tower-Defense-Game/Scripts/tower1Arrow.gd
@@ -1,16 +1,18 @@
 extends CharacterBody2D
 
-var target
+#changed this to acire target
+var target: Vector2 = Vector2.ZERO
+
 var Speed = 1000
 var pathName = ""
-var bulletDamage
+var bulletDamage = 100
 
 func _physics_process(delta):
-	var pathSpawnerNode = get_node("/root/Spawner")
+	var pathSpawnerNode = get_node("Spawner")
 	
-	for i in range(pathSpawnerNode.get_child_count()):
-		if pathSpawnerNode.get_child(i).name == pathName:
-			target = pathSpawnerNode.get_child(i).get_child(0).get_child(0).global_position
+	#for i in range(pathSpawnerNode.get_child_count()):
+		#if pathSpawnerNode.get_child(i).name == pathName:
+			#target = pathSpawnerNode.get_child(i).get_child(0).get_child(0).global_position
 
 	velocity = global_position.direction_to(target) * Speed
 
@@ -20,5 +22,10 @@ func _physics_process(delta):
 
 func _on_area_2d_body_entered(body):
 	if "Slime" in body.name or "Bee" in body.name or "Wolf" in body.name or "Goblin" in body.name:
+		target = body.global_position
 		body.health -= bulletDamage
 		queue_free()
+#BROCK
+#arrow only fires once when emey enters area and misses so needed a timer to queue free
+func _on_timer_timeout():
+	queue_free()
