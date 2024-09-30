599e702e52d189e02a2eb42ddde012de522fc16f
Eric Liu <liueric@ecs.vuw.ac.nz>
2024-09-30 17:10:38 +1300

    Made some modification on tower1 so that the drag and drop controls work for it.

--- Tower-Defense-Game/Scripts/tower.gd
+++ Tower-Defense-Game/Scripts/tower.gd
@@ -16,6 +16,12 @@ var tower_levels = [
 	{"damage": 500, "sprite": "res://Sprites/towerlvl5.png"}
 ]
 
+var draggable = false
+var is_inside_dropable = false
+var body_ref
+var offset : Vector2
+var initialPos : Vector2
+
 func level_up():
 	if current_level < tower_levels.size() - 1:
 		current_level += 1
@@ -57,3 +63,44 @@ func _on_tower_body_entered(body):
 func _on_tower_body_exited(body):
 	currTargets = get_node("Tower").get_overlapping_bodies()
 	
+func _process(delta):
+	if draggable:
+		if Input.is_action_just_pressed("click"):
+			initialPos = global_position
+			offset = get_global_mouse_position() - global_position
+			ControlManager.is_dragging = true
+			
+		if Input.is_action_pressed("click"):
+			global_position = get_global_mouse_position()
+		elif Input.is_action_just_released("click"):
+			ControlManager.is_dragging = false
+			var tween = get_tree().create_tween()
+			if is_inside_dropable:
+				tween.tween_property(self,"position",body_ref.position,0.2).set_ease(Tween.EASE_OUT)
+			else:
+				tween.tween_property(self,"global_position",initialPos,0.2).set_ease(Tween.EASE_OUT)
+
+func _on_area_2d_mouse_entered():
+	if not ControlManager.is_dragging:
+		draggable = true
+		scale = Vector2(1.05, 1.05)
+
+
+func _on_area_2d_mouse_exited():
+	if not ControlManager.is_dragging:
+		draggable = false
+		scale = Vector2(1, 1)
+
+
+func _on_area_2d_body_entered(body):
+	if body.is_in_group("drop"):
+		is_inside_dropable = true
+		body.modulate = Color(Color.REBECCA_PURPLE, 1)
+		body_ref = body
+
+
+func _on_area_2d_body_exited(body):
+	if body.is_in_group("drop"):
+		is_inside_dropable = false
+		body.modulate = Color(Color.MEDIUM_PURPLE, 0.7)
+
