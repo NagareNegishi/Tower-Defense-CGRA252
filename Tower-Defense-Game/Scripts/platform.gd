6f16ab004fad60da57749ebbba00661eaef7c057
BrockChoy <brockieeboyy@gmail.com>
2024-10-06 17:28:25 +1300

    Fixed arrows they now correctly track enemies have done some work with the towers and platforms they now only aloow one tower to occupy and the tower can no longer be reomved from the platform after it has been placed

--- Tower-Defense-Game/Scripts/platform.gd
+++ Tower-Defense-Game/Scripts/platform.gd
@@ -1,6 +1,6 @@
 extends StaticBody2D
 
-
+var is_occupied = false
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	modulate = Color(Color.MEDIUM_PURPLE, 0.7)
@@ -9,7 +9,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if ControlManager.is_dragging:
-		visible = true
-	else:
-		visible = false
-		
+	# If dragging is occurring, change the color or size of the platform
+		if ControlManager.is_dragging:
+			modulate = Color(Color.REBECCA_PURPLE, 1)  # Highlight when dragging
+		else:
+			modulate = Color(Color.MEDIUM_PURPLE, 0.7)  # Reset color when not dragging
+
+func occupy_platform():
+	is_occupied = true
+	modulate = Color(Color.DARK_SLATE_BLUE, 1)
