7a990d9a61123c5e151f092f4cbf9ac956e3ac1e
BrockChoy <brockieeboyy@gmail.com>
2024-10-16 15:21:28 +1300

    Added music settings menu and edited the main menu backdrop, still need a settings menu for in game

---
+++ Tower-Defense-Game/Settings.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+#dymanic variables for valume control
+@export
+var bus_name: String
+var bus_index: int
+
+
+func _ready():
+		#set bus index for music
+	bus_index = AudioServer.get_bus_index("Master")
+	
+	
+	#connect volume slider 
+	$Control/HSlider.value_changed.connect(_on_h_slider_value_changed)
+
+func _on_menu_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Prototype/Main_Menu.tscn")
+
+
+
+func _on_h_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(bus_index, linear_to_db(value))
+
+
+func _on_check_button_toggled(toggled_on):
+	AudioServer.set_bus_mute(bus_index, not AudioServer.is_bus_mute(bus_index))
